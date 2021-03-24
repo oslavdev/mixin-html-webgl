38ad106d1825d1f5d108321a75e91d90ca88c6e2
Jaroslaw <yaroslav@xuver-services.com>
2021-03-24 12:32:15 +0200

    Upload

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
     output:
     {
         filename: 'bundle.[contenthash].js',
-        path: path.resolve(__dirname, '../dist')
+        path: path.resolve(__dirname, '../public')
     },
     devtool: 'source-map',
     plugins:
